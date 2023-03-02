f401f2b25beac3f5785d0f7836a36f85ec2a9773
tikuro <tikuro@gmail.com>
2023-03-02 12:22:52 -0500

    update 2/03/2023
    update

--- js/carga.js
+++ js/carga.js
@@ -19,10 +19,10 @@ function Boton()
     
 
         if(selectValue=="proyecto1" && clave=="123"){
-            document.getElementById("Cabezote").style.display="block";
             document.getElementById("Time").style.display="block";
             document.getElementById("Perfomance").style.display="block";
-            document.getElementById("Issues").style.display="block";
+            document.getElementById("Disclaimer").style.display="block";
+            document.getElementById("titulo").style.display="block";
           }
           else if(selectValue=="powerapps"){
             document.getElementById("PowerApps").style.display="block";
@@ -52,4 +52,32 @@ function Boton()
     }
   });
 }
-        
+
+function printDiv(main) {
+  var contenido= document.getElementById(main).innerHTML;
+  var contenidoOriginal= document.body.innerHTML;
+  
+  document.body.innerHTML = contenido;
+
+  window.print();
+
+  document.body.innerHTML = contenidoOriginal;
+}
+
+function ocultarClase() {
+var elementos_lateral = document.getElementsByClassName("sidebar-sticky");
+var elementos_header = document.getElementsByClassName("h2");
+var elementos_botones = document.getElementsByClassName("btn-group mr-2"); 
+for (var i=0; i<elementos_lateral.length; i++){
+    elementos_lateral[i].style.display="none";
+  };
+for (var i=0; i<elementos_header.length; i++){
+    elementos_header[i].style.display="none";
+  };
+for (var i=0; i<elementos_botones.length; i++){
+    elementos_botones[i].style.display="none";
+  };
+document.getElementById("clave").style.display="none";
+document.getElementById("selector_proyectos").style.display="none"      
+window.print();
+}
